897b72f60b9fd6d4fcdce92bfce4e00584854177
jude-san <judekhay@gmail.com>
2024-01-16 21:45:27 +0000

    another one

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
-            <p class="">Transcript Portal</p>
+            <!-- <p class="mx-auto">Transcript Portal</p> -->
         </x-slot>
 
         <!-- Session Status -->
